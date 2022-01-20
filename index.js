fe6f1c29f647988295049bd38671b4deb50f759a
Tyler Rhinehart <rhinehart22@gmail.com>
2022-01-20 11:59:08 -0700

    questions and data

---
+++ index.js
@@ -0,0 +1,13 @@
+const users = require('./users.json');
+
+// 1. Return list of all Jedi
+
+
+// 2. Return the number of Jedi and number of Sith
+
+
+
+// 3. Sort by faction, then by lastName, display just lastName
+
+
+// 4. Using only the results of question 3, transform each item to be the first letter of the firstName followed by the lastName, in all lowercase, i.e. lskywalker, kren, etc.
