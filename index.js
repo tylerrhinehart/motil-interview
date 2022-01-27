edca1b6d27ad4da276eaeda919bc0a8f6f2317b9
Tyler Rhinehart <rhinehart22@gmail.com>
2022-01-27 10:54:40 -0700

    add service question

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const users = require('./users.json');
 // 2. Return the number of Jedi and number of Sith
 
 
-
 // 3. Sort by faction, then by lastName, display just lastName
 
 
 // 4. Using only the results of question 3, transform each item to be the first letter of the firstName followed by the lastName, in all lowercase, i.e. lskywalker, kren, etc.
+
+
+// 5. Create a CRUD service for the users data
